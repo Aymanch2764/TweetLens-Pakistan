d0fa9d9a2edaff27feeea87c244c4c69bede897a
Alia <119353826+Alia-raza@users.noreply.github.com>
2025-12-10 21:18:51 +0500

    Fix formatting issues in api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -10,6 +10,7 @@ const api = axios.create({
   headers: {
     'Content-Type': 'application/json',
   }
+  
 });
 // Request interceptor for adding auth tokens if needed
 api.interceptors.request.use(
